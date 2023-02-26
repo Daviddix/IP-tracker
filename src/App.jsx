63a662e024b6d624a79a3054bb92439b4b79666e
Emmanuel Nsikan-David <en56434@gmail.com>
2023-02-26 17:16:13 +0100

    main functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
 import TopSection from './components/TopSection'
+import { MapContainer, TileLayer, Marker, Popup, useMap  } from 'react-leaflet'
 
 function App() {
+   
 
   return (
     <main>      
     <TopSection />
+
       <div className="bottom-section">
-    <p>Map goes here</p>
+      <MapContainer center={[9.025860 , 7.578330]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[9.025860 , 7.578330]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
       </div>
 
     </main>
