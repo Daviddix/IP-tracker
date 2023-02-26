8323062155dfa05c3a628ea01e9448381d002926
Emmanuel Nsikan-David <en56434@gmail.com>
2023-02-26 18:00:58 +0100

    added marker and geolocation features

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import './App.css'
 import TopSection from './components/TopSection'
 import { MapContainer, TileLayer, Marker, Popup, useMap  } from 'react-leaflet'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-   
+   const [long, setLong] = useState("")
+   const [lat, setLat] = useState("")
+   useEffect(()=>{
+    getLongitudeandLatitude()
+   })
+
+   function getLongitudeandLatitude(){
+    navigator.geolocation.getCurrentPosition((p)=>{
+      setLong(p.coords.longitude)
+      setLat(p.coords.latitude)
+    })
+   }
 
   return (
     <main>      
-    <TopSection />
+    <TopSection/>
 
       <div className="bottom-section">
-      <MapContainer center={[9.025860 , 7.578330]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[long, lat]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[9.025860 , 7.578330]}>
+  <Marker position={[long, lat]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
   </Marker>
-</MapContainer>
+      </MapContainer>
       </div>
 
     </main>
