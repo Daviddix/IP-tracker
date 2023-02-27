22af2d7ef2c8cf528d71ed3e9a22af0b20b7b0d1
Emmanuel Nsikan-David <en56434@gmail.com>
2023-02-27 16:33:03 +0100

    last test

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,34 @@ function App() {
    const [long, setLong] = useState("")
    const [lat, setLat] = useState("")
    const mapRef = useRef(null)
+  
    const myIcon = L.icon({
     iconUrl : "/src/assets/icon-location.svg",
     iconSize : [30, 35],
     iconAnchor: [15, 15]
    })
-
+   
    useEffect(()=>{
     mapRef.current?.setView([lat,long])
    }, [[lat,long]])
 
   return (
     <main>      
-    <TopSection
-    setLong={setLong}
-    setLat={setLat}
-    />
+    <TopSection setLong={setLong} setLat={setLat} />
 
       <div className="bottom-section">
-      <MapContainer 
-      ref={mapRef}
-      center={[lat, long]} 
-      zoom={53} 
-      scrollWheelZoom={true}>
+
+      <MapContainer ref={mapRef} center={[lat, long]} zoom={53} scrollWheelZoom={true}>
       <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker 
-  icon={myIcon}
-  position={[lat, long]}>
-    <Popup>
-     Your Location based on your IP address
-    </Popup>
-  </Marker>
-      </MapContainer>
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+    <Marker icon={myIcon} position={[lat, long]}>
+      <Popup>
+      Your Location based on your IP address
+      </Popup>
+    </Marker>
+    </MapContainer>
       </div>
 
     </main>
